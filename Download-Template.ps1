fc15690dfe1d1c4b357ff1dba0e10da6c30974da
nathanharrison345 <nathan.harrison@345.technology>
2024-07-18 15:35:17 +0100

    Ready to deploy

--- Download-Template.ps1
+++ Download-Template.ps1
@@ -19,3 +19,51 @@ if ((test-path .\Template\$Win10FileName) -eq $true) {
 else {
     Invoke-WebRequest -Uri $Win10Url -OutFile ".\Template\$Win10FileName" -UseBasicParsing
 }
+
+
+# setup the variables
+# the first four need to match Enable-Idenity.ps1 script
+# destination image resource group
+$imageResourceGroup = 'aib-testing-rg'
+$location = (Get-AzResourceGroup -Name $imageResourceGroup).Location
+# Get your current subscription
+$subscriptionID = (Get-AzContext).Subscription.Id
+# name of the image to be created
+$imageName = 'aibCustomImageWin10'
+# image template name
+$imageTemplateName = 'imageTemplateWin10Multi'
+# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$runOutputName = 'Win10Client'
+# Set the Template File Path
+$templateFilePath = ".\Template\$Win10FileName"
+# user assigned managed identity
+$identityName = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup).Name
+# get the user assigned identity id
+$identityNameResourceID = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
+
+
+# Update the template
+((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>', $subscriptionID) | Set-Content -path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region>', $location) | Set-Content -path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>', $runOutputName) | Set-Content -path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imageName>', $imageName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<identityNameResourceId>', $identityNameResourceID) | Set-Content -Path $templateFilePath
+
+
+
+# Find the publisher, offer and sku
+# To use for the deployment template to identify 
+# source marketplace images
+# https://www.ciraltos.com/find-skus-images-available-azure-rm/
+Get-AzVMImagePublisher -Location $location | where-object {$_.PublisherName -like "*win*"} | ft PublisherName, Location
+$pubName - 'MicrosoftWindowsDesktop'
+Get-AzImageOffer -Location $location -PublisherName $pubName | ft Offer, PublisherName, Location
+# Set offer to 'office-365' for images with O365
+# $offerName = 'office-365'
+$offerName = 'Windows-10'
+Get-AzVMImageSku -Location $location -PublisherName $pubName -Offer $offerName | ft Skus, Offer, PublisherName, Location
+$skuName = '20h1-evd'
+Get-AzVMImage -Location $location -PublisherName $pubName -Skus $skuName -Offer $offerName
+$version = '19041.572.2010091946'
+Get-AzVMImage -Location $location -PublisherName $pubName -Offer $offerName -Skus $skuName -Version $version
